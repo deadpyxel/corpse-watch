f6464d7fce2224a063267e2ced54f6e0d7ac1d45
Robson Cruz <ghost0092@gmail.com>
2024-11-22 20:16:19 -0300

    feat(pkg/scanner): add parseLinks utility
    This new function will be used to parse the links inside an HTML body
    from a request response. The idea is that it will tokenize the contents
    of an HTTP response using HTML rules, pulling out the links contained on
    the `href` property of the `a` tag.
    
    Also added the utility functions `isBrowsableURL` (to check if an URL is
    browsable), `makeAbsoluteURL` (to convert relative URLs into absolute
    URLs). Added tests for both functions.

--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -1,7 +1,11 @@
 package scanner
 
 import (
+	"net/http"
 	"net/url"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 // IsSameDomain checks if two URLs belong to the same domain.
@@ -18,4 +22,50 @@ func IsSameDomain(baseURL, checkURL string) (bool, error) {
 	}
 
 	return base.Hostname() == check.Hostname(), nil
+}
+
+func isBrowsableURL(url string, invalidPrefixes []string) bool {
+	for _, prefix := range invalidPrefixes {
+		if strings.HasPrefix(url, prefix) {
+			return false
+		}
+	}
+	return true
+}
+
+func makeAbsoluteURL(baseURL *url.URL, href string) string {
+	relURL, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+	absURL := baseURL.ResolveReference(relURL)
+	return absURL.String()
+}
+
+func parseLinks(resp *http.Response) []string {
+	foundURLs := make([]string, 0)
+	tokenizer := html.NewTokenizer(resp.Body)
+
+	for {
+		tokenType := tokenizer.Next()
+		if tokenType == html.ErrorToken {
+			break
+		}
+
+		token := tokenizer.Token()
+		if tokenType == html.StartTagToken && token.Data == "a" {
+			for _, attr := range token.Attr {
+				link := attr.Val
+				invalidPrefixes := []string{"#", "javascript:", "mailto:", "tel:"}
+				if isBrowsableURL(link, invalidPrefixes) {
+					absoluteURL := makeAbsoluteURL(resp.Request.URL, link)
+					if absoluteURL != "" {
+						foundURLs = append(foundURLs, absoluteURL)
+					}
+				}
+			}
+		}
+	}
+
+	return foundURLs
 }
--- pkg/scanner/scanner_test.go
+++ pkg/scanner/scanner_test.go
@@ -3,6 +3,7 @@ package scanner
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"testing"
 )
 
@@ -70,4 +71,65 @@ func ExampleIsSameDomain() {
 	fmt.Printf("url1: %s and url2: %s belong to the same domain? %v", url1, url2, isSame)
 	// Output:
 	// url1: https://example.com and url2: https://example.com/page belong to the same domain? true
+}
+
+func TestIsBrowsableURL(t *testing.T) {
+	tests := []struct {
+		name            string
+		url             string
+		invalidPrefixes []string
+		expected        bool
+	}{
+		{
+			name:            "when given a url without any invalid prefix returns true",
+			url:             "https://www.example.com",
+			invalidPrefixes: []string{"http://", "ftp://"},
+			expected:        true,
+		},
+		{
+			name:            "when given a url with an invalid prefix returns false",
+			url:             "ftp://example.com",
+			invalidPrefixes: []string{"http://", "ftp://"},
+			expected:        false,
+		},
+		{
+			name:            "when given a empty slice of invalid prefixes returns true",
+			url:             "http://example.com",
+			invalidPrefixes: []string{},
+			expected:        true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := isBrowsableURL(test.url, test.invalidPrefixes)
+			if result != test.expected {
+				t.Errorf("For URL %s and invalid prefixes %v, expected %t but got %t", test.url, test.invalidPrefixes, test.expected, result)
+			}
+		})
+	}
+}
+
+func TestMakeAbsoluteURL(t *testing.T) {
+	baseURL, _ := url.Parse("https://www.example.com")
+	tests := []struct {
+		name    string
+		baseURL *url.URL
+		href    string
+		want    string
+	}{
+		{"relative path", baseURL, "/page1", "https://www.example.com/page1"},
+		{"absolute URL", baseURL, "https://www.example.com/page2", "https://www.example.com/page2"},
+		{"relative path not formatted", baseURL, "page3", "https://www.example.com/page3"},
+		{"invalid path", baseURL, ":invalid", ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := makeAbsoluteURL(tt.baseURL, tt.href)
+			if got != tt.want {
+				t.Errorf("makeAbsoluteURL(%s, %s) results in %s; want %s,", tt.baseURL, tt.href, got, tt.want)
+			}
+		})
+	}
 }
